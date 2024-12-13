fa8b7845d722c4beec47165f6fdb1ac04af16d23
Salami Abdullahi Programmer <salamiabdullahi247@gmail.com>
2024-12-13 03:39:43 +0100

    Create basic_operations.py
    created the first python file

---
+++ basic_operations.py
@@ -0,0 +1,4 @@
+number1 = 10
+number2 = 5
+
+print("Addition of ${number1} and ${number2} is ${number1 + number2}\nSubtraction of ${number1} and ${number2} is ${number1 - number2}\nAddition of ${number1} and ${number2} is ${number1 ' number2}")
