4a6118eb396fa26835b761ff4da6aa815bfc5b3c
Salami Abdullahi Programmer <salamiabdullahi247@gmail.com>
2024-12-16 05:28:54 +0100

    Delete basic_operations.py

--- basic_operations.py
+++
@@ -1,4 +0,0 @@
-number1 = 10
-number2 = 5
-
-print("Addition of ${number1} and ${number2} is ${number1 + number2}\nSubtraction of ${number1} and ${number2} is ${number1 - number2}\nAddition of ${number1} and ${number2} is ${number1 ' number2}")
